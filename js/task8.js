c51404161feee6d5d49d42dc7ea2a0d0d90d252e
Chois <sterchois@gmail.com>
2017-06-30 14:20:52 +0800

    task8

---
+++ js/task8.js
@@ -0,0 +1,79 @@
+
+'use strict'
+
+const app={};
+
+{
+    /**
+    * 简化版querySelector
+    * @param {*} ele
+    */
+    function $(ele){
+        return document.querySelector(ele);
+    }
+
+    const storeArr=[];
+    const textArea=$("textarea");
+    const insertBtn=$("#insertBtn");
+    const searchBtn=$("#searchBtn");
+    const searchInput=$("#searchInput");
+    const container=$("#container");
+    const clearBtn=$("#clearBtn");
+
+    function inputToArr(){
+        let ProcessedIput=textArea.value.trim().split(/[\n，,、\s+]/);
+        for(let item of ProcessedIput){
+            storeArr.push(item);
+        }
+    }
+
+    function search(){
+        let searchValue=searchInput.value;
+        let divArr=container.children;
+        function isMatch(text,item){
+            let re=RegExp(text);
+            if(re.test(item.innerText)){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        for(let item of divArr){
+            if(isMatch(searchValue,item)){
+                item.className=item.className+" "+"related";
+            }
+        }
+    }
+
+    function clearNode(){
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+    }
+
+    function render(){
+        clearNode();
+        for(let item of storeArr){
+            let newDiv=document.createElement("div");
+            newDiv.className="showdiv";
+            newDiv.innerText=item;
+            container.appendChild(newDiv);
+        }
+    }
+
+    function addHandler(){
+        insertBtn.addEventListener("click",inputToArr);
+        insertBtn.addEventListener("click",render);
+        searchBtn.addEventListener("click",render);
+        searchBtn.addEventListener("click",search);
+        clearBtn.addEventListener("click",clearNode);
+    }
+
+    function init(){
+    addHandler();
+    }
+    app.init=init;
+}
+
+app.init();
+
--- js/temp.js
+++
@@ -1,9 +0,0 @@
-let arr=[1,2,3,4,5];
-let length=arr.length;
-function test(){
-    for(let i=1;i<=length;i++){
-        arr.pop();
-    }
-}
-
-test();
