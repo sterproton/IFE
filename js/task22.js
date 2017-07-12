3ebb9f16731f2f9d9d6a1d6a7c48dcd48c935f7a
Chois <sterchois@gmail.com>
2017-07-12 14:39:43 +0800

    task22

--- js/task22.js
+++ js/task22.js
@@ -1,19 +1,71 @@
 
 'use strict'
 
+const app={};
+{
+
+}
 function $(ele){
     return document.querySelectorAll(ele);
 }
+
 const container=$("#container")[0];
 const binaryTreeGenerateBtn=$("button")[0];
 const input=$("input")[0];
-const BinaryTreeTraverBtn=$("button")[1];
+const binaryTreeTraveralBtn=$("button")[1];
+
+function isPair(node){
+    if(node.children.length>0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function NodesTraversing(node,func,param){
+    if (isPair(node)) {
+        func(node,param);
+        NodesTraversing(node.firstElementChild,func,param);
+        NodesTraversing(node.lastElementChild,func,param);
+    }else{
+        func(node,param);
+    }
+}
+
+function LeafTraversing(node,func,param){
+    if(isPair(node)){
+        LeafTraversing(node.firstElementChild,func,param);
+        LeafTraversing(node.lastElementChild,func,param);
+    }else{
+        func(node,param);
+    }
+}
+
+function PairNodeTraversingByNum(node,num,func){
+    if(num>1){
+        if (isPair(node)) {
+            PairNodeTraversingByNum(node.firstElementChild,num-1,func);
+            PairNodeTraversingByNum(node.lastElementChild,num-1,func);
+        }else{
+            func(node);
+            PairNodeTraversingByNum(node.firstElementChild,num-1,func);
+            PairNodeTraversingByNum(node.lastElementChild,num-1,func);
+        }
+    }
+}
+
+function createBinaryTree(node,num){
+    PairNodeTraversingByNum(node,num,createChildNode);
+}
+
 function getChildWidth(parent){
     return (parent.offsetWidth-22)/2-12;
 }
+
 function getChildHeight(parent){
     return parent.offsetHeight-44;
 }
+
 function createChildNode(parent){
     let leftNode=document.createElement("div");
     let rightNode=document.createElement("div");
@@ -24,64 +76,19 @@ function createChildNode(parent){
     leftNode.style.width=rightNode.style.width=getChildWidth(parent)+'px';
     leftNode.style.height=rightNode.style.height=getChildHeight(parent)+'px';
 }
+
 function clearChildNode(node){
     while(node.hasChildNodes()){
         node.removeChild(node.firstChild);
     }
 }
-function createBinaryTree(node,num){
-    clearChildNode(node);
-    if(num>1){
-        if(num>=6){
-            throw Error("宽度不够");
-        }
-        //elementBinaryTreeAllNodesTraveralFunc(node,createBinaryTree,num-1);
-        if (isPair(node)) {
-            createBinaryTree(node.firstElementChild,num-1);
-            createBinaryTree(node.lastElementChild,num-1);
-        }else{
-            createChildNode(node);
-            createBinaryTree(node.firstElementChild,num-1);
-            createBinaryTree(node.lastElementChild,num-1);
-        }
-    }
-}
 
-function isPair(node){
-    if(node.children.length>0){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-let cnt=0;
-function elementBinaryTreeLeafTraveralFunc(node,func,param){
-    if(isPair(node)){
-        elementBinaryTreeLeafTraveralFunc(node.firstElementChild,func,param);
-        elementBinaryTreeLeafTraveralFunc(node.lastElementChild,func,param);
-    }else{
-        func(node,param);
-    }
-}
-
-function elementBinaryTreeAllNodesTraveralFunc(node,func,param){
-    if (isPair(node)) {
-        func(node,param);
-        elementBinaryTreeAllNodesTraveralFunc(node.firstElementChild,func,param);
-        elementBinaryTreeAllNodesTraveralFunc(node.lastElementChild,func,param);
-    }else{
-        func(node,param);
-    }
-}
-function redBorder(node){
-    node.style.borderColor="red";
-}
 function backgroundToWhite(node){
     node.style.backgroundColor="#fff";
 }
+
 function changeBackgroundColor(node){
-    elementBinaryTreeAllNodesTraveralFunc(container,backgroundToWhite);
+    NodesTraversing(container,backgroundToWhite);
     node.style.backgroundColor="red";
     if(isPair(node)){
         node.firstElementChild.style.backgroundColor="#fff";
@@ -89,21 +96,32 @@ function changeBackgroundColor(node){
     }
 }
 
-
-function btnHandler(){
-    createBinaryTree(container,parseInt(input.value));
+function checkInput(inputValue){
+    let value=Number.parseInt(inputValue)
+    return !Number.isNaN(value)&&(0<value&&value<6);
 }
-function traveralHandler(){
-    elementBinaryTreeAllNodesTraveralFunc(container,function(node){ 
+
+function generaetHandler(){
+    if(checkInput(input.value)){
+        clearChildNode(container);
+        createBinaryTree(container,parseInt(input.value));
+    }else{
+        alert("非法输入或者0<二叉树层数<6");
+        return false;
+    }
+}
+function traverallHandler(){
+    let cnt=0;
+    NodesTraversing(container,function(node){ 
         cnt++;
-        function wraper(){
+        function wrap(){
             changeBackgroundColor(node);
         }
-        setTimeout(wraper,cnt*150);
-    })
-    cnt=0;
+        setTimeout(wrap,cnt*300);
+    });
 }
 createBinaryTree(container,5);
-binaryTreeGenerateBtn.addEventListener("click",btnHandler);
-BinaryTreeTraverBtn.addEventListener("click",traveralHandler);
+binaryTreeGenerateBtn.addEventListener("click",generaetHandler);
+binaryTreeTraveralBtn.addEventListener("click",traverallHandler);
+
 
